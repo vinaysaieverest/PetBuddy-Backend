d421c74dd9e3d9e6fc351acd3a7339c3e58507d0
Admin <admin@Admins-MacBook-Pro-8.local>
2024-11-05 14:31:38 +0530

    Implement the route for login

---
+++ routes/loginRoute.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { Login } from '../services/login';
+const express = require("express")
+const router = express.Router();
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name,password} = req.body as { name:string,password:string};
+      const user = new Login (name , password)
+      const results = await user.setLogin()
+      res.status(200).send("Login Success")
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  module.exports = router;
