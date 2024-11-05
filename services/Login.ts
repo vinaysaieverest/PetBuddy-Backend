c5a61f93e7f55b4328643a7addfe639d1fbdce99
Admin <admin@Admins-MacBook-Pro-8.local>
2024-11-05 14:57:07 +0530

    Update Login function

--- services/Login.ts
+++ services/Login.ts
@@ -1,23 +1,29 @@
 import { dataSchema } from "../model";
+import { Response ,Request} from "express";
 export class Login {
-    name:string;
+    username:string;
     password:string;
     constructor(name: string, password: string) {
-        this.name = name;
+        this.username = name;
         this.password = password;
     }
      async setLogin(){
         try{
-            const name =this.name;
+            const username =this.username;
             const password = this.password 
-            const user = await dataSchema.findOne({ name});
+            const user = await dataSchema.findOne({ username});
             if (!user) {
-              return false;
+              return {status:400} ;
             }
-            if(user.password===password){
-              console.log("login",name)
-              return true ;
+            if(user.password===password && user.username===username){
+              return  {status:200} ; ;
             }
+            if(user.username===username){
+                return  {status:401} ; ;
+              }
+              if(user.password===password){
+                return  {status:401} ; ;
+              }
         }
         catch(e){
             throw new Error("An error occurred while login");
